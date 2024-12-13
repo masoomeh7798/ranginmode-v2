dab659dd9c79fde61db639d5a12952d8f827bcb7
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-12-13 12:03:49 +0330

    widgets design completed

--- admin/src/Components/Widget/index.jsx
+++ admin/src/Components/Widget/index.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import "./style.scss"
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
 
-export default function Widget() {
-  return (
-    <div className='widget'>
-        <div className="right">
-            <span className="title">كاربران</span>
-            <span className="counter">86574</span>
-            <span className="link">مشاهده همه كاربران</span>
+export default function Widget({ type }) {
+    let data;
+    let amount = 1000
+    switch (type) {
+        case 'user':
+            data = {
+                title: "كاربران",
+                isMoney: false,
+                link: "مشاهده همه كاربران",
+                icon: <PersonOutlineOutlinedIcon className='icon' sx={{color:'var(--secondary-clr)',bgcolor:'rgba(255,0,0,.2)'}}/>
+            }
+            break;
+        case 'order':
+            data = {
+                title: "سفارشات",
+                isMoney: false,
+                link: "مشاهده همه سفارشات",
+                icon: <ShoppingCartOutlinedIcon className='icon' sx={{color:'#BA8E23',bgcolor:'rgba(255,255,0,.2)'}} />
+            }
+            break;
+        case 'earning':
+            data = {
+                title: "درآمد",
+                isMoney: true,
+                link: "مشاهده كل درآمد",
+                icon: <MonetizationOnOutlinedIcon className='icon'  sx={{color:'green',bgcolor:'rgba(0,255,0,.2)'}} />
+            }
+            break;
+        case 'balance':
+            data = {
+                title: 'تراز',
+                isMoney: true,
+                link: "مشاهده جزئيات",
+                icon: <AccountBalanceWalletOutlinedIcon className='icon'  sx={{color:'purple',bgcolor:'rgba(160,32,240,.2)'}} />
+            }
+            break;
+        default: break;
+    }
+    return (
+        <div className='widget'>
+            <div className="right">
+                <span className="title">{data.title}</span>
+                <span className="counter">{amount} {data.isMoney && 'ت'}</span>
+                <span className="link">{data.link}</span>
+            </div>
+            <div className="left">
+                {data.icon}
+            </div>
         </div>
-        <div className="left">
-            <AccountBoxIcon className='icon'/>
-        </div>
-    </div>
-  )
+    )
 }
--- admin/src/Pages/Home/index.jsx
+++ admin/src/Pages/Home/index.jsx
@@ -11,10 +11,10 @@ export default function Home() {
         <div className="homeContainer">
             <Navbar/>
             <div className="widgets">
-                <Widget/>
-                <Widget/>
-                <Widget/>
-                <Widget/>
+                <Widget type={'user'}/>
+                <Widget type={'order'}/>
+                <Widget type={'earning'}/>
+                <Widget type={'balance'}/>
             </div>
         </div>
        </div>
