8c909cc6d45667d3083eae9cc93aa8463c1b1b05
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-12-13 20:46:45 +0330

    `Added @mui/x-data-grid package and implemented a DataGrid component in the Datatable component`

--- admin/src/Components/Datatable/index.jsx
+++ admin/src/Components/Datatable/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import "./style.scss"
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns, userRows } from '../../datatableSource.jsx';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import CloseIcon from '@mui/icons-material/Close';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+
+const paginationModel = { page: 0, pageSize:8 };
+
+const actionColumn = [
+    {
+        field: "action",
+        headerName: "گزينه ها",
+        width: 200,
+        renderCell: (params) => {
+            return (
+                <div className="actionButtons">
+                    <span className="viewBtn">
+                        <VisibilityIcon />
+                    </span>
+                    <span className="editBtn">
+                        <ModeEditOutlineIcon />
+                    </span>
+                    <span className="deleteBtn">
+                        <CloseIcon />
+                    </span>
+                </div>
+            )
+        }
+    }
+]
+
+
 
 export default function Datatable() {
-  return (
-    <div className='datatable'>Datatable</div>
-  )
+    return (
+        <div className='datatable'>
+            <DataGrid
+                rows={userRows}
+                columns={userColumns.concat(actionColumn)}
+                initialState={{ pagination: { paginationModel } }}
+                checkboxSelection
+            />
+        </div>
+    )
 }
