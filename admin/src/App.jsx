c451a3a11afb07b3ec6341ef200d9f0a3c7e0fae
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-12-14 14:59:06 +0330

    create users and products in admin panel designed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,6 +6,7 @@ import NotFound from './Pages/NotFound/index.jsx';
 import List from './Pages/List/index.jsx';
 import Single from './Pages/Single/index.jsx';
 import New from './Pages/New/index.jsx';
+import { productInputs, userInputs } from './formSource.js';
 
 
 
@@ -17,10 +18,10 @@ export default function App() {
       <Route path='*' element={<NotFound/>}/>
       <Route path='users' element={<List/>} />
       <Route path='users/:userId' element={<Single/>} />
-      <Route path='users/new' element={<New/>} />
+      <Route path='users/new' element={<New inputs={userInputs} title="افزودن كاربر"/>} />
       <Route path='products' element={<List/>} />
       <Route path='products/:productId' element={<Single/>} />
-      <Route path='products/new' element={<New/>} />
+      <Route path='products/new' element={<New inputs={productInputs} title="افزودن محصول"/>} />
     </Routes>
 
   )
